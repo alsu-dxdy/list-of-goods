068c08b022efe468ec7f9a6c394179a11fa887be
Alsu Abdrakhmanova <dxdy10@mail.ru>
2021-01-23 14:45:03 +0500

    fix: url image in goods

--- src/js/constants.js
+++ src/js/constants.js
@@ -20,7 +20,7 @@ const goods = [
       "current_price": 355
     },
     "image": {
-      "url": "https://2.downloader.disk.yandex.ru/preview/14029c1c94a220c6a80dcf22f0435949beeb499bacf005b47a7bc2546a40f04f/inf/sAZbD3pXyGVo4yLASmZP5AjZtAjV2NRRHQlbGQoD9KFNU13aoA73KnI7yCZC7jSvcUHlKt1-Sq2Htqz-iyVZqg%3D%3D?uid=199298657&filename=pic1.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=199298657&tknv=v2&size=1295x622",
+      "url": "https://i.ibb.co/BzbZSD2/pic1.png",
     },
     "material": 1
   },
@@ -33,7 +33,7 @@ const goods = [
       "current_price": 355.555
     },
     "image": {
-      "url": "https://3.downloader.disk.yandex.ru/preview/335de239f9c736096084a3160f557a0299535b2e342aafd4b0d131e46f670579/inf/Vbtdfei8s7LMftSGwUhLpQjZtAjV2NRRHQlbGQoD9KEBxCulokZIxdcol_dYICg7fsG9cxTxZBH5ejdXeU587Q%3D%3D?uid=199298657&filename=pic2.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=199298657&tknv=v2&size=1295x622"
+      "url": "https://i.ibb.co/JsHKy3q/pic2.png",
     },
     "material": 2
   },
@@ -46,7 +46,7 @@ const goods = [
       "current_price": 75
     },
     "image": {
-      "url": "https://3.downloader.disk.yandex.ru/preview/1ef63a40969ee8a72606e359e7cc934f6eb227ab10b4f96c969da3608cbd6cde/inf/xlaKEUcwF0PoQpZcpAAiw29fDf9MzVaCKwH3qeSZaFjajEtT0pb-fSuWYwD7LOWxFKAdY1WyFkF886e-4v2joQ%3D%3D?uid=199298657&filename=pic3.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=199298657&tknv=v2&size=1295x622"
+      "url": "https://i.ibb.co/RSnKDhv/pic3.png",
     },
     "material": 2
   },
@@ -59,7 +59,7 @@ const goods = [
       "current_price": 200
     },
     "image": {
-      "url": "https://1.downloader.disk.yandex.ru/preview/92705c7c9222178d9aa30decb78dddb6dd0e877b61ce4065aef2b1b34d166ae2/inf/-P7ECL0QKNSXglx6-VfmnL9BuhgoLmKL-J8ShhpF8MAdZ0j_Kos-FoRIo0Z3NQNCWAWlFrgE7_yYTr-Cp6ebaw%3D%3D?uid=199298657&filename=pic4.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=199298657&tknv=v2&size=1295x622"
+      "url": "https://i.ibb.co/nMYtBx6/pic4.png",
     },
     "material": 2
   },
@@ -72,7 +72,7 @@ const goods = [
       "current_price": 720
     },
     "image": {
-      "url": "https://2.downloader.disk.yandex.ru/preview/14029c1c94a220c6a80dcf22f0435949beeb499bacf005b47a7bc2546a40f04f/inf/sAZbD3pXyGVo4yLASmZP5AjZtAjV2NRRHQlbGQoD9KFNU13aoA73KnI7yCZC7jSvcUHlKt1-Sq2Htqz-iyVZqg%3D%3D?uid=199298657&filename=pic1.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=199298657&tknv=v2&size=1295x622"
+      "url": "https://i.ibb.co/BzbZSD2/pic1.png",
     },
     "material": 1
   },
@@ -85,7 +85,7 @@ const goods = [
       "current_price": 355.555
     },
     "image": {
-      "url": "https://3.downloader.disk.yandex.ru/preview/335de239f9c736096084a3160f557a0299535b2e342aafd4b0d131e46f670579/inf/Vbtdfei8s7LMftSGwUhLpQjZtAjV2NRRHQlbGQoD9KEBxCulokZIxdcol_dYICg7fsG9cxTxZBH5ejdXeU587Q%3D%3D?uid=199298657&filename=pic2.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=199298657&tknv=v2&size=1295x622"
+      "url": "https://i.ibb.co/JsHKy3q/pic2.png",
     },
     "material": 2
   },
@@ -98,7 +98,7 @@ const goods = [
       "current_price": 75
     },
     "image": {
-      "url": "https://3.downloader.disk.yandex.ru/preview/1ef63a40969ee8a72606e359e7cc934f6eb227ab10b4f96c969da3608cbd6cde/inf/xlaKEUcwF0PoQpZcpAAiw29fDf9MzVaCKwH3qeSZaFjajEtT0pb-fSuWYwD7LOWxFKAdY1WyFkF886e-4v2joQ%3D%3D?uid=199298657&filename=pic3.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=199298657&tknv=v2&size=1295x622"
+      "url": "https://i.ibb.co/RSnKDhv/pic3.png",
     },
     "material": 2
   },
@@ -111,7 +111,7 @@ const goods = [
       "current_price": 900
     },
     "image": {
-      "url": "https://1.downloader.disk.yandex.ru/preview/92705c7c9222178d9aa30decb78dddb6dd0e877b61ce4065aef2b1b34d166ae2/inf/-P7ECL0QKNSXglx6-VfmnL9BuhgoLmKL-J8ShhpF8MAdZ0j_Kos-FoRIo0Z3NQNCWAWlFrgE7_yYTr-Cp6ebaw%3D%3D?uid=199298657&filename=pic4.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=199298657&tknv=v2&size=1295x622"
+      "url": "https://i.ibb.co/nMYtBx6/pic4.png",
     },
     "material": 2
   }
